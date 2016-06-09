69f529ab75c12cc61092efe00463a7e8026ff7d1
laure <ga41sfl->
2016-06-09 21:23:12 +0200

    DB connection fix

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'themannetdadmin');
+define('DB_NAME', 'themannetdwp');
 
 /** MySQL database username */
-define('DB_USER', 'themannetdadmin');
+define('DB_USER', 'themannetdwp');
 
 /** MySQL database password */
 define('DB_PASSWORD', '8JFmRFsPPAy6');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'mysql55-55.perso');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
