7a59a5af40462278906735eaec0955833483c817
laure <ga41sfl->
2016-07-25 20:52:23 +0200

    Construction page template creation

---
+++ wp-content/themes/twentysixteen/tpl-under-construction.php
@@ -0,0 +1,18 @@
+<?php /* Template Name: Under Construction Template */ ?>
+<?php get_header() ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+				<header>
+					<h1><?php the_title(); ?></h1>
+				</header>
+
+                <section>
+                    <?php the_content(); ?>
+                </section>
+
+		</main><!-- .site-main -->
+	</div><!-- .content-area -->
+
+<?php get_footer(); ?>
