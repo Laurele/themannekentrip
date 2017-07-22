2a96175491c9933434c9434761ea11b97f9f849d
laure <laure@homelog.be>
2017-07-22 02:09:50 +0200

    Homepage : styling parallax fix

--- wp-content/themes/twentysixteen/tpl-index.php
+++ wp-content/themes/twentysixteen/tpl-index.php
@@ -7,7 +7,7 @@
 
 <div class="parallax">
     <section id="group1" class="parallax__group">
-        <div class="parallax__layer parallax__layer--base">
+        <div class="parallax__layer parallax__layer--fore">
             <div class="box header">
                 <div class="header-content">
                     <h1><?php bloginfo('name'); ?></h1>
@@ -15,24 +15,11 @@
                 </div>
             </div>
         </div>
-        <div class="parallax__layer parallax__layer--back background-paper-parallax">
+        <div class="parallax__layer parallax__layer--base background-paper-parallax">
             <div class="box background-map"></div>
         </div>
     </section>
     <section id="group2" class="parallax__group">
-        <div class="parallax__layer parallax__layer--deep">
-            <div class="box gallery">
-                <?php $i = 0; ?>
-                <?php while ($i < 6) : ?>
-                    <span class="gallery-item">
-                    <img src="http://lorempixel.com/100/100">
-                </span>
-                    <?php $i++; ?>
-                <?php endwhile; ?>
-            </div>
-        </div>
-    </section>
-    <section id="group3" class="parallax__group">
         <div class="parallax__layer parallax__layer--base">
             <div class="box">
                 <?php $myPosts = get_posts(
@@ -57,8 +44,22 @@
                 <?php endforeach; ?>
             </div>
         </div>
+        <div class="parallax__layer parallax__layer--back">
+            <div class="box gallery">
+                <?php $i = 0; ?>
+                <?php while ($i < 6) : ?>
+                    <span class="gallery-item">
+                    <img src="http://lorempixel.com/400/400">
+                </span>
+                    <?php $i++; ?>
+                <?php endwhile; ?>
+            </div>
+        </div>
     </section>
-    <section id="group4" class="parallax__group">
+    <section id="group3" class="parallax__group">
+        <div class="parallax__layer parallax__layer--fore">
+            <div class="title">Foreground Layer</div>
+        </div>
         <div class="parallax__layer parallax__layer--base">
             <div class="box">
                 <?php $myPosts = get_posts(
@@ -73,7 +74,7 @@
                     ]
                 ); ?>
                 <?php foreach ($myPosts as $post) : setup_postdata($post); ?>
-                    <!-- TODO : implement the stats page link ? -->
+                    <!-- TODO : implement the stats page link -->
                     <h3>
                         <a href="#"><?php the_title(); ?></a>
                     </h3>
@@ -82,6 +83,38 @@
                     </p>
                 <?php endforeach; ?>
             </div>
+        </div>
+    </section>
+    <section id="group4" class="parallax__group">
+        <div class="parallax__layer parallax__layer--base">
+            <div class="title">Base Layer</div>
+        </div>
+        <div class="parallax__layer parallax__layer--back">
+            <div class="title">Background Layer</div>
+        </div>
+        <div class="parallax__layer parallax__layer--deep">
+            <div class="title">Deep Background Layer</div>
+        </div>
+    </section>
+    <section id="group5" class="parallax__group">
+        <div class="parallax__layer parallax__layer--fore">
+            <div class="title">Foreground Layer</div>
+        </div>
+        <div class="parallax__layer parallax__layer--base">
+            <div class="title">Base Layer</div>
+        </div>
+    </section>
+    <section id="group6" class="parallax__group">
+        <div class="parallax__layer parallax__layer--back">
+            <div class="title">Background Layer</div>
+        </div>
+        <div class="parallax__layer parallax__layer--base">
+            <div class="title">Base Layer</div>
+        </div>
+    </section>
+    <section id="group7" class="parallax__group">
+        <div class="parallax__layer parallax__layer--base">
+            <div class="title">Base Layer</div>
         </div>
     </section>
 </div>
