c5cdfe76683f3fad85077dcff08ec0b8a7dd64a9
laure <laure@homelog.be>
2017-09-14 22:53:26 +0200

    Post : meta posted translation fix

--- wp-content/themes/themannekentrip/content-post-meta.php
+++ wp-content/themes/themannekentrip/content-post-meta.php
@@ -5,7 +5,7 @@
     if ( !$format ) { ?>
         
         <div class="entry-meta entry-header">
-            <span class="published"><?php echo pll__('Posted'); ?><?php echo human_time_diff( get_the_time('U'), current_time('timestamp') ) . pll__('ago'); ?>&middot;</span>
+            <span class="published">&middot;<?php echo pll__('Posted'); ?><?php echo human_time_diff( get_the_time('U'), current_time('timestamp') ) . pll__('ago'); ?>&middot;</span>
             <span class="entry-tags"><?php the_tags('&nbsp;'.__('Tagged:', 'zilla').' &middot; ', ', ', ''); ?></span>
         </div>
         
@@ -17,7 +17,7 @@
                 <?php if( !is_single() ) { ?>
                     <a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php printf(__('Permanent Link to %s', 'zilla'), get_the_title()); ?>"><?php echo __('Posted ', 'zilla') . human_time_diff( get_the_time('U'), current_time('timestamp') ) . __(' ago', 'zilla'); ?></a>
                 <?php } else {
-                    echo pll__('Posted') . human_time_diff( get_the_time('U'), current_time('timestamp') ) . pll__('ago');
+                    echo '&middot;' . pll__('Posted') . human_time_diff( get_the_time('U'), current_time('timestamp') ) . pll__('ago') . '&middot;';
                 } ?>
             </span>
         </div>
